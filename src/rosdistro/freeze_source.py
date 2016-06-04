3b71703a053b90d77510dd034baecc4366f2e879
Mike Purvis <mpurvis@clearpathrobotics.com>
2016-06-03 21:43:06 -0400

    Restore converting tags to hashes.

--- src/rosdistro/freeze_source.py
+++ src/rosdistro/freeze_source.py
@@ -91,11 +91,10 @@ def _worker(work_queue):
             ls_remote_lines = subprocess.check_output(cmd).splitlines()
             for line in ls_remote_lines:
                 hash, ref = line.split('\t', 1)
-                if ref.endswith('refs/tags/%s' % freeze_version) and freeze_to_tag:
+                if ref == 'refs/tags/%s' % freeze_version and freeze_to_tag:
                     source_repo.version = ref.split('refs/tags/')[1]
                     break
-                elif ref.endswith('refs/heads/%s' % freeze_version):
-                    # Don't bother changing to a hash if it's already a tag.
+                elif ref == 'refs/heads/%s' % freeze_version or ref == 'refs/tags/%s' % freeze_version:
                     source_repo.version = hash
                     break
 
